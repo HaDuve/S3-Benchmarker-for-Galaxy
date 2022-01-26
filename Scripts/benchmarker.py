c02955a5df7b066f9c776aef9ded4432fe549aeb
HaDuve <48185176+HaDuve@users.noreply.github.com>
2022-01-26 12:01:52 +0100

    debug

--- Scripts/benchmarker.py
+++ Scripts/benchmarker.py
@@ -22,8 +22,13 @@ def report_time(fn, arg1 = None, arg2 = None, arg3 = None, iter=1):
     if(arg1 and arg2 and arg3): argstr = str(arg1) + ',' + str(arg2) + ',' + str(arg3)
     if(iter==None): iter = 1
     
+    #debug
+    print('argstr: ', argstr)
+    print('fn: ', fn)
+    
     # timing
     time = timeit.timeit(fn + "("+argstr+")", setup="from functions import " + fn + "; gc.enable()", number=iter)
+
     
     # output handling    
     print(fn + ": " + str(round(time,3)) + " sec for " + str(iter) + " iterations")
