c4d7b493943536bd99ba8b6286044c2d8cc38a4f
HaDuve <48185176+HaDuve@users.noreply.github.com>
2022-01-29 14:34:05 +0100

    debug file.tell

--- Scripts/utils.py
+++ Scripts/utils.py
@@ -84,10 +84,11 @@ def afterBenchmark(args):
     if(args.function == 'seekPOSIX'):
         if(args.arg1 is not None):
             with open(args.arg1, 'r') as file:
-                print(file.tell())
+                print('file.tell(): ', file.tell())
+
         else: #use test.txt
             with open("test.txt", 'r') as file:
-                print(file.tell())
+                print('file.tell(): ', file.tell())
             
 def checkPlatform(ComparePlatform):
     """[compares platforms and returns true if match, false otherwise]
