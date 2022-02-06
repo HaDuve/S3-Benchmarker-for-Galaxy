05c0813a59469d69ee27e32f738dbf2ce870e3ce
HaDuve <hionoe@hotmail.de>
2022-02-06 17:15:18 +0100

    added testing multiple functions at once via config (#5)

--- Scripts/runbench.py
+++ Scripts/runbench.py
@@ -15,24 +15,50 @@ if __name__=='__main__':
     
     work = config['Workflow']
     workflow = work['workflow']
+    workflowargs = work['workflowargs']
     result_path = work['result_path']
     warmup = work['warmup']
-    
-    #default namespace
-    args =  Namespace(arg1=None,
-                      arg2=None,
-                      arg3=None,
-                      cleanup=False,
-                      function='debug',
-                      r=None,
-                      saveas='data',
-                      default_region=default_region,
-                      s3_url=s3_url,
-                      s3_access_key=s3_access_key,
-                      s3_secret_key=s3_secret_key,
-                      result_path=result_path,
-                      warmup=warmup)
-    
-    #TODO: workflow handling    
-    workflow=workflow
-    _ = benchmarker.Benchmarker(args)
+    #workflow
+    workflowlist = list(workflow.split(","))
+    workflowargslist = list(workflowargs.split(","))
+    i = 1
+    while (workflowlist):        
+        filename = "data"+str(i)
+        next_function = workflowlist.pop(0)
+        next_arglist = workflowargslist.pop(0)
+        next_arglist = list(next_arglist.split(""))
+        if (next_arglist):
+            if len(next_arglist == 1):
+                arg1 = next_arglist[0]
+                arg2 = None
+                arg3 = None
+            if len(next_arglist == 2):
+                arg1 = next_arglist[0]
+                arg2 = next_arglist[1]
+                arg3 = None
+            if len(next_arglist == 3):
+                arg1 = next_arglist[0]
+                arg2 = next_arglist[1]
+                arg3 = next_arglist[2]
+        else:
+            arg1 = arg2 = arg3 = None
+        print(arg1,arg2, arg3)
+            
+        #default namespace + config values
+        args =  Namespace(arg1=arg1,
+                        arg2=arg2,
+                        arg3=arg3,
+                        cleanup=False,
+                        function=next_function,
+                        r=None,
+                        saveas=next_function,
+                        default_region=default_region,
+                        s3_url=s3_url,
+                        s3_access_key=s3_access_key,
+                        s3_secret_key=s3_secret_key,
+                        result_path=result_path,
+                        warmup=warmup)
+        
+        #TODO: workflow handling    
+        workflow=workflow
+        _ = benchmarker.Benchmarker(args)
